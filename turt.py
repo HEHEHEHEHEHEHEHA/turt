33d542281a57ee8f5e8409b59be595546b5c5128
HEHEHEHEHEHEHEHA <connor.kilmer9@gmail.com>
2025-09-18 10:42:58 -0400

    Update turt.py

--- turt.py
+++ turt.py
@@ -1,3 +1,4 @@
+
 #Connor Kilmer-Setrakian
 #9/15/25
 #turt
@@ -9,14 +10,14 @@ import turtle
 screen = turtle.Screen()
 my_turtle = turtle.Turtle()
 my_turtle.pendown()
-
+def bullet():
+    turt = turtle.Turtle()
+    for i in range(30):
+        turt.forward(20)
 # Define the movement functions
 def move_forward():
     my_turtle.forward(4)
     screen.ontimer(move_forward, 20)
-
-def move_backward():
-    my_turtle.backward(10)
 
 def turn_left():
     my_turtle.left(15)
@@ -28,14 +29,12 @@ def turn_right():
 screen.listen()
 
 # Bind the keys to the functions
-screen.onkey(move_forward, "Up")
-screen.onkey(move_backward, "Down")
 screen.onkey(turn_left, "Left")
 screen.onkey(turn_right, "Right")
+screen.onkey(bullet, "Down")
 
 #Makes Turtle Move
 move_forward()
 
 # Keep the window open
 turtle.done()
-
